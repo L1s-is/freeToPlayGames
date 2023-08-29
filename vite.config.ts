943695d7d6b152e24c2acf69830d1f3e09a8a4ef
Elizaveta Gorbatova <gev@d-element.ru>
2023-08-29 23:38:43 +0500

    updated boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: "/index.html",
+    port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$injectedColor: orange;`,
+      },
+      devSourcemap: true,
+    },
+  },
 })
